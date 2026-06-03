beb2d6c7a0aa5f9f8e4e2bcfe4c98164702a65c9
Bryan Braggion Pimentel de Lima <bryanbraggionpimentel@hotmail.com>
2026-06-03 22:54:03 +0000

    versao2

--- react_vazio/vazio/App.js
+++ react_vazio/vazio/App.js
@@ -1,36 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.red}>just red</Text>
-      <Text style={styles.bigBlue}>just bigBlue</Text>
-      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
-    </View>
-    <View style={styles.teste}>
-      <Text style={styles.red}>just red</Text>
-      <Text style={styles.bigBlue}>just bigBlue</Text>
-      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.titulo}>
+        <Text style={styles.titulo_texto}>Titulo app</Text>
+      </View>
+
+      <View>
+        <TextInput style={styles.input_estilo}></TextInput>
+      </View>
+
+      <view>
+        <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}} style={{width: 200, height: 200}}/>
+      </view>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-    width: '66%',
-    height: '35%',
-    backgroundColor: 'steelblue',
+  titulo: {
+    marginTop: 20,
+    width: 200,
+    alignSelf: 'center',
+    backgroundColor: 'black',
+    borderColor: 'yellow',
+    borderWidth: 2,
   },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
+  titulo_texto: {
     color: 'red',
+    textAlign: 'center',
+  },
+  input_estilo: {
+    marginTop:20,
+    borderWidth: 2,
+    borderColor: 'black',
+    width: 200,
+    alignSelf: 'center',
   },
 });
